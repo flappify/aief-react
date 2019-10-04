519259b274f2fefae8ccdfcf9267cf5e8d6cdc93
saahilshaikh <saahilshaikhrocks@gmail.com>
2019-10-05 00:11:09 +0530

    04102019

--- src/actions/formActions.js
+++ src/actions/formActions.js
@@ -1,5 +1,7 @@
 export const updateForm =(form)=>{
     return(dispatch,getState,{getFirebase,getFirestore})=>{
+        const firestore=getFirestore();
+        firestore.collection('forms')
         dispatch({type:'UPDATE_FORM',form});
     }
 };
